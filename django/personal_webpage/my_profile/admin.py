a051a94c09f4dc08ccfbf1e0dabc87ac5d361ee9
Godwin <65491585+gkrampah@users.noreply.github.com>
2023-05-15 19:55:06 +0200

    delete

--- django/personal_webpage/my_profile/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from my_profile.models import About
-
-# Register your models here.
-admin.site.register(About)
--- django/personal_webpage/my_profile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.homepage, name="index"),
-    path("about/", views.about_view, name="about_index"),
-    path("publications/", views.publication_view, name="publications_index"),
-    
-]
